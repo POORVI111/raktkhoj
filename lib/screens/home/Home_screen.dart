f6fb6e51aa90d8f1c73357fb9c1fa1fbe00938f5
POORVI111 <poorvigarg16@gmail.com>
2021-10-14 13:43:39 +0530

    backend request blood

---
+++ lib/screens/home/Home_screen.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:raktkhoj/components/ripple_indicator.dart';
+import 'package:raktkhoj/screens/home/map_view.dart';
+
+import '../../Colors.dart';
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  User currentUser;
+  String _name, _bloodgrp, _email;
+  Widget _child;
+
+  Future<Null> _fetchUserInfo() async {
+    Map<String, dynamic> _userInfo;
+    User _currentUser = await FirebaseAuth.instance.currentUser;
+
+    DocumentSnapshot _snapshot = await FirebaseFirestore.instance
+        .collection('User Details')
+        .doc(_currentUser.uid)
+        .get();
+
+    _userInfo = _snapshot.data();
+
+    this.setState(() {
+      _name = _userInfo['Name'];
+      _email = _userInfo['Email'];
+      _bloodgrp = _userInfo['BloodGroup'];
+      _child = _myWidget();
+    });
+  }
+
+
+  Future<void> _loadCurrentUser() async {
+    User _currentUser;
+    _currentUser = await FirebaseAuth.instance.currentUser;
+    setState(() {
+      currentUser =_currentUser;
+    });
+
+  }
+
+  @override
+  Future<void> initState() {
+    _child = RippleIndicator("Fetching Map");
+     _loadCurrentUser();
+    _fetchUserInfo();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, //top bar color
+      systemNavigationBarColor: Colors.black, //bottom bar color
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+    return _child;
+  }
+  Widget _myWidget() {
+    return Scaffold(
+      backgroundColor: kMainRed,
+      appBar: AppBar(
+        elevation: 0.0,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          "Home",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontFamily: "SouthernAire",
+            color: Colors.white,
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0.0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: kMainRed,
+              ),
+              accountName: Text(
+                currentUser == null ? "" : _name,
+                style: TextStyle(
+                  fontSize: 22.0,
+                ),
+              ),
+              accountEmail: Text(currentUser == null ? "" : _email),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text(
+                  currentUser == null ? "" : _bloodgrp,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.black54,
+                    fontFamily: 'SouthernAire',
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text("Home"),
+              leading: Icon(
+                FontAwesomeIcons.home,
+                color: kMainRed,
+              ),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomePage()));
+              },
+            ),
+            ListTile(
+              title: Text("Blood Donors"),
+              leading: Icon(
+                FontAwesomeIcons.handshake,
+                color: kMainRed,
+              ),
+              onTap: () {
+                // Navigator.push(context,
+                //     MaterialPageRoute(builder: (context) => DonorsPage()));
+              },
+            ),
+            ListTile(
+              title: Text("Blood Requests"),
+              leading: Icon(
+                FontAwesomeIcons.burn,
+                color: kMainRed,
+              ),
+              onTap: () {
+                //
+              },
+            ),
+            ListTile(
+              title: Text("Campaigns"),
+              leading: Icon(
+                FontAwesomeIcons.ribbon,
+                color: kMainRed,
+              ),
+              onTap: () {
+                // Navigator.push(context,
+                //     MaterialPageRoute(builder: (context) => CampaignsPage()));
+              },
+            ),
+            ListTile(
+              title: Text("Logout"),
+              leading: Icon(
+                FontAwesomeIcons.signOutAlt,
+                color: kMainRed,
+              ),
+              onTap: () async {
+                // await FirebaseAuth.instance.signOut();
+                // Navigator.pushReplacement(
+                //     context,
+                //     MaterialPageRoute(
+                //         builder: (context) => AuthPage(FirebaseAuth.instance)));
+              },
+            ),
+          ],
+        ),
+      ),
+      body: ClipRRect(
+        borderRadius: new BorderRadius.only(
+            topLeft: const Radius.circular(40.0),
+            topRight: const Radius.circular(40.0)),
+        child: Container(
+          height: 800.0,
+          width: double.infinity,
+          color: Colors.white,
+          child: MapView(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/request_blood.dart
@@ -0,0 +1,333 @@
+import 'package:dropdownfield/dropdownfield.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geolocator/geolocator.dart';
+
+import '../../Colors.dart';
+import 'Home_screen.dart';
+
+class RequestBlood extends StatefulWidget {
+  double _lat, _lng;
+  RequestBlood(this._lat, this._lng);
+  @override
+  _RequestBloodState createState() => _RequestBloodState();
+}
+
+class _RequestBloodState extends State<RequestBlood> {
+  final formkey = new GlobalKey<FormState>();
+  List<String> _bloodGroup = ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'];
+  List<String> _condition=['critical', 'normal'];
+  String _selected = '';
+  String _qty;
+  String _phone;
+  String _address;
+  String _patientCondition;
+  String _userID;
+  bool _categorySelected = false;
+  DateTime selectedDate = DateTime.now();
+  var formattedDate;
+  int flag = 0;
+  User currentUser;
+  List<Placemark> placemark;
+  @override
+  void initState() {
+    super.initState();
+    _loadCurrentUser();
+    getAddress();
+  }
+
+  bool isLoggedIn() {
+    if (FirebaseAuth.instance.currentUser != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<void> addData(_user) async {
+    if (isLoggedIn()) {
+      FirebaseFirestore.instance
+          .collection('Blood Request Details')
+          .doc(_user['uid'])
+          .set(_user)
+          .catchError((e) {
+        print(e);
+      });
+    } else {
+      print('You need to be logged In');
+    }
+  }
+
+  Future<void> _loadCurrentUser() async {
+    User currentUser;
+    currentUser = await FirebaseAuth.instance.currentUser;
+    setState(() {
+      currentUser =currentUser;
+      _userID= currentUser.uid;
+    });
+
+  }
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: selectedDate,
+        lastDate: DateTime(2022));
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+        flag = 1;
+      });
+    var date = DateTime.parse(selectedDate.toString());
+    formattedDate = "${date.day}-${date.month}-${date.year}";
+  }
+
+  Future<bool> dialogTrigger(BuildContext context) async {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Success'),
+            content: Text('Blood Request Submitted'),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  formkey.currentState.reset();
+                  Navigator.pop(context);
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => HomePage()));
+                },
+                child: Icon(
+                  Icons.arrow_forward,
+                  color: kMainRed,
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
+  void getAddress() async {
+    placemark =
+    await Geolocator().placemarkFromCoordinates(widget._lat, widget._lng);
+    _address = placemark[0].name.toString() +
+        "," +
+        placemark[0].locality.toString() +
+        ", Postal Code:" +
+        placemark[0].postalCode.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kMainRed,
+      appBar: AppBar(
+        elevation: 0.0,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          "Request Blood",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontFamily: "SouthernAire",
+            color: Colors.white,
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            FontAwesomeIcons.reply,
+            color: Colors.white,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: ClipRRect(
+        borderRadius: new BorderRadius.only(
+            topLeft: const Radius.circular(40.0),
+            topRight: const Radius.circular(40.0)),
+        child: Container(
+          height: 800.0,
+          width: double.infinity,
+          color: Colors.white,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                new Form(
+                  key: formkey,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.only(top: 20.0),
+                              child: DropdownButton(
+                                hint: Text(
+                                  'Please choose a Blood Group',
+                                  style: TextStyle(
+                                    color: kMainRed,
+                                  ),
+                                ),
+                                iconSize: 40.0,
+                                items: _bloodGroup.map((val) {
+                                  return new DropdownMenuItem<String>(
+                                    value: val,
+                                    child: new Text(val),
+                                  );
+                                }).toList(),
+                                onChanged: (newValue) {
+                                  setState(() {
+                                    _selected = newValue;
+                                    this._categorySelected = true;
+                                  });
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              _selected,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0,
+                                color: kMainRed,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Quantity(L)',
+                            icon: Icon(
+                              FontAwesomeIcons.prescriptionBottle,
+                              color: kMainRed,
+                            ),
+                          ),
+                          validator: (value) => value.isEmpty
+                              ? "Quantity field can't be empty"
+                              : null,
+                          onSaved: (value) => _qty = value,
+                          keyboardType: TextInputType.number,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: <Widget>[
+                            IconButton(
+                              onPressed: () => _selectDate(context),
+                              icon: Icon(FontAwesomeIcons.calendar),
+                              color: kMainRed,
+                            ),
+                            flag == 0
+                                ? Text(
+                              "<< Pick up a Due Date",
+                              style: TextStyle(
+                                  color: Colors.black54, fontSize: 15.0),
+                            )
+                                : Text(formattedDate),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: DropDownField(
+                          controller: TextEditingController(),
+                          itemsVisibleInDropdown: 2,
+                          icon: Icon(FontAwesomeIcons.hospitalUser,color: kMainRed,),
+                          hintText: "Patient Condition",
+                          hintStyle: TextStyle(color: Colors.black54, fontSize: 15.0),
+                          enabled: true,
+                          items:_condition,
+                          onValueChanged: (value){
+                            _patientCondition=value;
+
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Phone Number',
+                            icon: Icon(
+                              FontAwesomeIcons.mobile,
+                              color: kMainRed,
+                            ),
+                          ),
+                          validator: (value) => value.isEmpty
+                              ? "Phone Number field can't be empty"
+                              : null,
+                          onSaved: (value) => _phone = value,
+                          maxLength: 10,
+                          keyboardType: TextInputType.number,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: <Widget>[
+                            IconButton(
+                              onPressed: null,
+                              icon: Icon(FontAwesomeIcons.paperclip),
+                              color: kMainRed,
+                            ),
+                            Text(
+                              "<< Add a valid medical report",
+                              style: TextStyle(
+                                  color: Colors.black54, fontSize: 15.0),
+                            )
+                            
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      RaisedButton(
+                        onPressed: () {
+                          if (!formkey.currentState.validate()) return;
+                          formkey.currentState.save();
+                          final Map<String, dynamic> BloodRequestDetails = {
+                            'uid': _userID,
+                            'bloodGroup': _selected,
+                            'quantity': _qty,
+                            'dueDate': formattedDate,
+                            'phone': _phone,
+                            'location': new GeoPoint(widget._lat, widget._lng),
+                            'address': _address,
+                            'patientCondition': _patientCondition
+                          };
+                          addData(BloodRequestDetails).then((result) {
+                            dialogTrigger(context);
+                          }).catchError((e) {
+                            print(e);
+                          });
+                        },
+                        textColor: Colors.white,
+                        padding: EdgeInsets.only(left: 5.0, right: 5.0),
+                        color: kMainRed,
+                        child: Text("SUBMIT"),
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(30.0)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
