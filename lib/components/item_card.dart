b1ed3dccd5338ab549aa2cdbc6377da9657f892b
Radhika gupta <60813363+radhika2312@users.noreply.github.com>
2021-12-26 23:26:27 +0530

    Update item_card.dart

--- lib/components/item_card.dart
+++ lib/components/item_card.dart
@@ -1,3 +1,4 @@
+//card provider for request and donations
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:raktkhoj/constants.dart';
